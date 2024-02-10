e631f6822f072ea61d8fa09b023010b1075df9d8
KRISHNA K <kkrishna22572@gmail.com>
2024-02-10 10:54:17 +0530

    [refactor] : formatted code

--- src/components/component/Reviews.tsx
+++ src/components/component/Reviews.tsx
@@ -16,18 +16,24 @@ import { useNavigate } from "react-router-dom";
 import Comments from "./Comments";
 import Cookies from "js-cookie";
 interface ReviewsProps {
-    book: GoogleBook;
+  book: GoogleBook;
 }
 
 const Reviews: React.FC<ReviewsProps> = ({ book }) => {
-    const [reviews, setReviews] = useState<Review[]>([]);
-    const [userReview, setUserReview] = useState({ rating: 4.3, text: "" });
-    const {user } = useSelector((state:RootState) => state.auth);
-    const navigate = useNavigate();
-    const accessToken = Cookies.get("access_token");
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [userReview, setUserReview] = useState({ rating: 4.3, text: "" });
+  const { user } = useSelector((state: RootState) => state.auth);
+  const navigate = useNavigate();
+  const accessToken = Cookies.get("access_token");
   const fetchReviews = async () => {
     try {
-      const response = await axios.get(`${config.url}/api/reviews/${book.id}`,{headers:{ 'Content-Type' : 'application/json', "Authorization" : `Bearer ${accessToken}`},withCredentials:true}); // Replace with your backend URL
+      const response = await axios.get(`${config.url}/api/reviews/${book.id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        withCredentials: true,
+      }); // Replace with your backend URL
       console.log(response.data);
       setReviews(response.data.reviews);
     } catch (error: any) {
@@ -40,15 +46,21 @@ const Reviews: React.FC<ReviewsProps> = ({ book }) => {
 
   const handleReviewSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    if(!user || !accessToken){
-        console.log("postreview",user,accessToken);
-        return navigate('/login');
+    if (!user || !accessToken) {
+      console.log("postreview", user, accessToken);
+      return navigate("/login");
     }
     try {
       await axios.post(
         `${config.url}/api/reviews/${book.id}`, // Replace with your backend URL
-        {...userReview,userId:user._id,username:user.username},
-        {headers:{ 'Content-Type' : 'application/json','Authorization' : `Bearer ${accessToken}`},withCredentials:true}
+        { ...userReview, userId: user._id, username: user.username },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          withCredentials: true,
+        }
       );
 
       // After submitting a review, fetch updated reviews
@@ -60,6 +72,8 @@ const Reviews: React.FC<ReviewsProps> = ({ book }) => {
       console.error("Error submitting review:", error.message);
     }
   };
+
+  const [commentsOpen, setCommentsOpen] = useState(false);
 
   return (
     <section className="relative bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
@@ -95,96 +109,87 @@ const Reviews: React.FC<ReviewsProps> = ({ book }) => {
         </form>
         {reviews.map((review) => (
           <>
-          <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
-            <footer className="flex justify-between items-center mb-2">
-              <div className="flex items-center">
-                <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                  <img
-                    className="mr-2 w-6 h-6 rounded-full"
-                    src={`avatar2.jpg`}
-                    alt="Michael Gough"
+            <article className="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+              <footer className="flex justify-between items-center mb-2">
+                <div className="flex items-center">
+                  <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                    <img
+                      className="mr-2 w-6 h-6 rounded-full"
+                      src={`avatar2.jpg`}
+                      alt="Michael Gough"
                     />
-                  {review.username}
-                </p>
-                <p className="text-sm text-gray-600 dark:text-gray-400">
-                  <time>{`${new Date(review.updatedAt)}`}</time>
-                </p>
-              </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <button
-                    id="dropdownComment2Button"
-                    data-dropdown-toggle="dropdownComment2"
-                    className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    type="button"
+                    {review.username}
+                  </p>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    <time>{`${new Date(review.updatedAt)}`}</time>
+                  </p>
+                </div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <button
+                      id="dropdownComment2Button"
+                      data-dropdown-toggle="dropdownComment2"
+                      className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-40 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                      type="button"
                     >
-                    <svg
-                      className="w-4 h-4"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 16 3"
+                      <svg
+                        className="w-4 h-4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 16 3"
                       >
-                      <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                    </svg>
-                    <span className="sr-only">Comment settings</span>
-                  </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="">
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      comment
-                      <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Edit
-                      <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Remove
-                      <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </footer>
-            <p className="text-gray-500 dark:text-gray-400">
-              Very straight-to-point article. Really worth time reading. Thank
-              you! But tools are just the instruments for the UX designers. The
-              knowledge of the design tools are as important as the creation of
-              the design strategy.
-            </p>
-            <div className="flex items-center mt-4 space-x-4">
-              <button
-                type="button"
-                className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
+                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                      </svg>
+                      <span className="sr-only">Comment settings</span>
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="">
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>
+                        Edit
+                        <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        Remove
+                        <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </footer>
+              <p className="text-gray-500 dark:text-gray-400">{review.text}</p>
+              <div className="flex items-center mt-4 space-x-4">
+                <button
+                  type="button"
+                  onClick={() => setCommentsOpen((old) => !old)}
+                  className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
                 >
-                <svg
-                  className="mr-1.5 w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 18"
+                  <svg
+                    className="mr-1.5 w-3.5 h-3.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 18"
                   >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
                     />
-                </svg>
-                Reply
-              </button>
-            </div>
-          </article>
-          <Comments review={review}/>
-        </>
+                  </svg>
+                  Reply
+                </button>
+              </div>
+            </article>
+            {commentsOpen && <Comments review={review} />}
+          </>
         ))}
       </div>
     </section>
-    );
-  };
-  
-  export default Reviews;
-  
+  );
+};
+
+export default Reviews;
